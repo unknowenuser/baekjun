07b9388d887a5baf75fbdd0cb4aa104b57a985f5
unknowenuser <a40966361@gmail.com>
2025-07-06 01:25:53 +0900

    prob1오류 수정

--- prob1.py
+++ prob1.py
@@ -2,27 +2,31 @@ def get_data(row,col):
     result = []
     for i in range(col):
         data = int(input())
-        divder = 10
         row_data = []
         for j in range(row):
             row_data.append(data%10)
-            data = data // divder
+            data = data // 10
         result.append(row_data)
     return result
 def get_biggest_rect(row,col,data):
-    biggest_rect = 0
-    for i in range(col):
-        for j in range(row):
-            for k in range(i,col):
-                for l in range(j,row):
-                    value1 = data[i][j]
-                    value2 = data[k][l]
-                    value3 = data[i][l]
-                    value4 = data[k][j]
-                    if value1 == value2 and value1 == value3 and value1 == value4:
-                        size = (k-i + 1)*(l-j + 1)
-                        if size > biggest_rect:
-                            biggest_rect = size
+    biggest_rect = 1
+    for i in range(col-1):
+        for j in range(row-1):
+            if row - j <= col - i:
+                max_distance = row - j
+                smaller = "row"
+            else :
+                max_distance = col - i
+                smaller = "col"
+            for k in range(1,max_distance):
+                value1 = data[i][j]
+                value2 = data[i][j + k]
+                value3 = data[i+k][j]
+                value4 = data[i+k][j+k]
+                if value1 == value2 and value1 == value3 and value1 == value4:
+                    size = (k + 1) * (k + 1)
+                    if size > biggest_rect:
+                        biggest_rect = size
     return biggest_rect
 
 grid_col,grid_row = input().split()
