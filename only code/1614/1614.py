8435b1c3cadba20d7b8938c15c1aeac736d96ff0
unknowenuser <a40966361@gmail.com>
2025-07-11 21:05:56 +0900

    1614 py 알고리즘 변경

--- only code/1614/1614.py
+++ only code/1614/1614.py
@@ -1,16 +1,12 @@
 finger_num = int(input())
 countable = int(input())
-count = 0
-while countable !=0 :
-    if (count//4)%2 == 0:
-        if finger_num != 5:
-            countable -= 1
-        count += 4
-    else :
-        if finger_num != 1 :
-            countable -= 1
-        count += 4
-if (count//4)%2 == 0:
-    print(count + finger_num - 1)
+if finger_num != 1 and finger_num != 5:
+    count = (countable//2) * 8
+    if countable %2 == 1:
+        count = count + 9 -finger_num
+    else:
+        count = count + finger_num - 1
 else:
-    print(count - finger_num + 5)
+    count = countable * 8 + finger_num - 1
+    countable = 0
+print(count)
