5ce07db5c405cb5eef8cff884ce14c9ed6d24938
unknowenuser <a40966361@gmail.com>
2025-07-29 18:05:40 +0900

    2502 완성

--- only code/2502/2502.cpp
+++ only code/2502/2502.cpp
@@ -7,9 +7,9 @@ int main() {
     f1_bef = 1;
     f1_now = 0;
     f1_aft = 1;
-    f1_bef = 0;
-    f1_now = 1;
-    f1_aft = 1;
+    f2_bef = 0;
+    f2_now = 1;
+    f2_aft = 1;
     cin >> t >> nt;
     for (int i = 3; i < t; i++)
     {
@@ -20,18 +20,17 @@ int main() {
         f2_now = f2_aft;
         f2_aft = f2_bef + f2_now;
     }
-    int first  = 0,second = 0;
-    while (nt != f1_aft*first + f2_aft*second)
+    int first  = 0,second,calc_value = 0;
+    while (calc_value != nt)
     {
         first++;
-        for (second = 0; nt != f1_aft*first + f2_aft*second; second++)
+        second = first;
+        calc_value = f1_aft*first + f2_aft*second;
+        for (; nt > calc_value; second++)
         {
-            if (nt == f1_aft*first + f2_aft*second)
-            {
-                break;
-            }
+            calc_value = f1_aft*first + f2_aft*second;
         }
     }
-    cout << first << endl << second;
+    cout << first << endl << second - 1;
     return 0;
 }
