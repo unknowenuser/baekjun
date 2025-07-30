5d4d54ef84a8c0beb2160a044f88cc6ee6a7a390
unknowenuser <a40966361@gmail.com>
2025-07-30 18:30:44 +0900

    16395 py완성

---
+++ only code/16395/16395.py
@@ -0,0 +1,18 @@
+class pascal:
+    def combination(self,a,b):
+        result = 1
+        if b*2 > a:
+            b = a-b
+        for i in range(b):
+            result *= (a - i - 1)
+        for i in range(b):
+            result = result // (i + 1)
+        return result
+    
+    def __init__(self,a,b):
+        self.num = self.combination(a,b)
+    def print(self):
+        print(self.num)
+a,b = map(int,input().split())
+num = pascal(a,b)
+num.print()
