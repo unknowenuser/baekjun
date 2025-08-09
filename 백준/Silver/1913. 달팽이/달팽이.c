fd07b0b84bc09bea640ead903ea2f81e3a8ea72b
unknowenuser <61786848+unknowenuser@users.noreply.github.com>
2025-08-09 18:05:44 +0900

    [Silver III] Title: 달팽이, Time: 128 ms, Memory: 4896 KB -BaekjoonHub

---
+++ 백준/Silver/1913. 달팽이/달팽이.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,index;
+    scanf("%d",&n);
+    scanf("%d",&index);
+    int snail[n][n], x = n / 2, y = n / 2,start = n / 2,i = 0;
+    snail[x][y] = i + 1;
+    i++;
+    int dir[6] = {0,-1,0,1,0,-1};
+    while(i != n*n)
+    {
+        if (x == y && x <= n / 2)
+        {
+            y--;
+            snail[x][y] = i + 1;
+            i++;
+        }
+        int move_num = (start - y) * 2;
+        for (int j = 0; j < move_num - 1; j++)
+        {
+            x++;
+            snail[x][y] = i+1;
+            i++;
+        }
+        for (int j = 0; j < 3; j++)
+        {
+            for (int k = 0; k < move_num; k++)
+            {
+                x += dir[j];
+                y += dir[j+3];
+                snail[x][y] = i+1;
+                i++;
+            }
+        }
+    }
+
+    for (int k = 0; k < n; k++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ",snail[j][k]);
+            if (snail[j][k] == index)
+            {
+                x = j+1;
+                y = k+1;
+            }
+            
+        }
+        printf("\n");
+    }
+    printf("%d %d",y,x);
+    return 0;
+}
