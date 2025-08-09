bb7894f89764aac86c694d706dfb840fb9bf7efa
unknowenuser <61786848+unknowenuser@users.noreply.github.com>
2025-08-09 20:25:10 +0900

    1913 c 완성

--- only code/1913/1913.c
+++ only code/1913/1913.c
@@ -2,34 +2,50 @@
 
 int main()
 {
-    int n;
+    int n,index;
     scanf("%d",&n);
-    int snail[n][n], x = (n + 1) / 2, y = (n + 1) / 2, index = 1,start = (n + 1) / 2,i = 0;
-    snail[x][y] = index;
-    index += 1;
-    int dir[8] = {1,0,-1,0,0,-1,0,1};
+    scanf("%d",&index);
+    int snail[n][n], x = n / 2, y = n / 2,start = n / 2,i = 0;
+    snail[x][y] = i + 1;
+    i++;
+    int dir[6] = {0,-1,0,1,0,-1};
     while(i != n*n)
     {
-        if (x == y && x <= (n + 1) / 2)
-        {
-            y++;
-            snail[x][y] = index;
-            index++;
-            i++;
-            continue;
-        }
+        y--;
+        snail[x][y] = i + 1;
+        i++;
         int move_num = (start - y) * 2;
-        for (int j = 0; j < 4; i++)
+        for (int j = 0; j < move_num - 1; j++)
+        {
+            x++;
+            snail[x][y] = i+1;
+            i++;
+        }
+        for (int j = 0; j < 3; j++)
         {
             for (int k = 0; k < move_num; k++)
             {
                 x += dir[j];
-                y += dir[j+4];
-                snail[x][y] = i;
+                y += dir[j+3];
+                snail[x][y] = i+1;
                 i++;
             }
         }
     }
-    
+
+    for (int k = 0; k < n; k++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ",snail[j][k]);
+            if (snail[j][k] == index)
+            {
+                x = j+1;
+                y = k+1;
+            }
+        }
+        printf("\n");
+    }
+    printf("%d %d",y,x);
     return 0;
 }
